468b97ca938dbb4a84e50872e80baa2ed770b885
Vasanth Venkatesh <sharmila.vasanth_venkatesh@siemens.com>
2018-08-13 23:31:26 +0200

    test commit

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+﻿var x = 0; 
+var s = "";
+console.log("hello word");
+var theform = document.getElementById("theform");
+theform.hidden = true;
+var button = document.getElementById("buy");
+button.addEventListener("click", function () {
+    console.log("buing item");
+
+});
+var productinfo = document.getElementsByClassName("product-props");
+var listitmes = productinfo.item[0].children;
+
